064e8e90b95189aa2e7dc3013fd456d7a19d8f2b
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-11-19 19:43:42 +0100

    Create views: - clients - client

--- carshop/views.py
+++ carshop/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
+
 
 # Create your views here.
+def clients(request):
+    all_clients = models.Client.objects.all()
+    return render(request, "clients.html", {"all_clients": all_clients})
+
+
+def client(request, client_id: int):
+    one_client = get_object_or_404(models.Client, id=client_id)
+    client_orders = models.Order.objects.filter(client=one_client)
+
+    context = {
+        "one_client": one_client,
+        "client_orders": client_orders,
+    }
+
+    return render(request, "client.html", {"context": context})
